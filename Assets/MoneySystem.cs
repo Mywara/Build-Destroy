1ee6de584b7e71f46816651f1c6639973f1b53ed
Cthulhu <maxime.cthulhu@gmail.com>
2018-03-02 22:57:08 -0500

    Base du système d'argent
    Lis le titre tabarnak

---
+++ Assets/MoneySystem.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneySystem : MonoBehaviour {
+
+    private static MoneySystem _instance;
+    public int money;
+    public  Text currency;
+    public int income = int.MaxValue;
+
+
+    private static MoneySystem instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                if (GameObject.Find("MoneySystem"))
+                {
+                    GameObject g = GameObject.Find("MoneySystem");
+                    if (g.GetComponent<MoneySystem>())
+                    {
+                        _instance = g.GetComponent<MoneySystem>();
+                    }
+                    else
+                    {
+                        _instance = g.AddComponent<MoneySystem>();
+                    }
+                }
+                else
+                {
+                    GameObject g = new GameObject();
+                    g.name = "MoneySystem";
+                    _instance = g.AddComponent<MoneySystem>();
+                }
+            }
+
+            return _instance;
+        }
+
+
+        set
+        {
+            _instance = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        //Make sure the Gameobject is named MoneySystem.
+        gameObject.name = "MoneySystem";
+        _instance = this;
+
+        //load the saved money
+        AddMoney(PlayerPrefs.GetInt("MoneySave", 0));
+
+        //Update Money
+        SaveMoney();
+    }
+
+
+
+    //while reality exists, save money every saveInterval.
+    public void SaveMoney()
+    {
+            PlayerPrefs.SetInt("MoneySave", instance.money);
+            currency.text = "Money : " + money.ToString() + "$";
+    }
+
+    //Checks if you have enough money to buy item with cost, if you do buy it and return true. Otherwise, return false.
+    public bool BuyItem(int cost)
+    {
+        if (instance.money - cost >= 0)
+        {
+            instance.money -= cost;
+            SaveMoney();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //Simply return the balance
+    public static int GetMoney()
+    {
+        return instance.money;
+    }
+
+    //Add some money to the balance.
+    public void AddMoney(int amount)
+    {
+        instance.money += amount;
+        SaveMoney();
+    }
+
+}
+
+
