2be19e2a4b09e62881c783ce3ccf1b2afb6ad04c
DarkXolotl <ghis7@hotmail.com>
2018-03-02 19:23:07 -0500

    Card System Created
    Card ScriptableObject backbone created

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour {
+
+    // Variable declarations
+    public Card card;
+
+    public Text costText;
+    public Image image;
+
+	// Use this for initialization
+	void Start () {
+        costText.text = card.cost;
+        image.sprite = card.image;
+	}
+}
